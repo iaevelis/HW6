ad3c81a744ea0c6725abb7a7468e2a8c9d7788c2
Jay <31777481+iaevelis@users.noreply.github.com>
2017-10-17 22:57:13 -0700

    Create frank_stella.js

---
+++ frank_stella.js
@@ -0,0 +1,14 @@
+function setup() { 
+  createCanvas(470, 400);
+} 
+
+function draw() { 
+  background(0);
+	noFill();
+	rectMode(CENTER);
+	stroke(255)
+		for (var a = 80; a < 1000; a+=20){
+
+			rect(width/2, height/2,a,a-70)
+		}
+}
